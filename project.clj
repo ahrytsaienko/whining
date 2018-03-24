c2feb01a26add94b9524896e5bb74f241baa42b2
a.hrytsaienko <a.hrytsaienko@modnakasta.ua>
2018-03-24 23:55:28 +0200

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject whining "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ring/ring-core "1.6.3"]
+                 [org.immutant/web "2.1.10"]
+                 [compojure "1.6.0"]
+                ; [javax.servlet/servlet-api "2.5"]
+                 [lein-ancient "0.6.15"]
+                 ])
+
---
+++ test/whining/core_test.clj
@@ -0,0 +1,7 @@
+(ns whining.core-test
+  (:require [clojure.test :refer :all]
+            [whining.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
